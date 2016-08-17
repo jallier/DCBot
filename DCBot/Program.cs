75b493717662f512365d8821f6710300fddc7372
Justin Hallier <justin.hallier+git@gmail.com>
2016-08-17 23:23:58 +1200

    Converted to local variables instead of class variables

--- DCBot/Program.cs
+++ DCBot/Program.cs
@@ -19,19 +19,17 @@ namespace DCBot
             new Program().Start();
         }
 
-        private Initializer config;
-        private DiscordClient _client;
-        private IAudioClient _vClient;
-        private Queue<Command> audioQueue = new Queue<Command>();
         private bool audioPlaying = false; //This seems like a dirty hack.
+        private bool IsRunningOnMono { get; } = (Type.GetType("Mono.Runtime") != null);
 
         /// <summary>
         /// Method to start the bot.
         /// </summary>
         public void Start()
         {
-            config = new Initializer();
-            _client = new DiscordClient(x => { x.LogLevel = LogSeverity.Info; });
+            Initializer config = new Initializer();
+            DiscordClient _client = new DiscordClient(x => { x.LogLevel = LogSeverity.Info; });
+            Queue<Command> audioQueue = new Queue<Command>();
 
             _client.Log.Message += (s, e) => Console.WriteLine($"[{e.Severity}] {e.Source}: {e.Message}");
 
@@ -48,7 +46,7 @@ namespace DCBot
                 x.HelpMode = HelpMode.Public;
             });
 
-            createCommands();
+            createCommands(config, _client, audioQueue);
 
             _client.ExecuteAndWait(async () =>
             {
@@ -69,7 +67,7 @@ namespace DCBot
         /// <summary>
         /// Create the commands from the commands in the config file for Discord.Net
         /// </summary>
-        private void createCommands()
+        private void createCommands(Initializer config, DiscordClient _client, Queue<Command> audioQueue)
         {
             foreach (Audio command in config.commands)
             {
@@ -80,8 +78,8 @@ namespace DCBot
                {
                    if (e.User.VoiceChannel != null)
                    {
-                       addAudioToQueue(command.Path, e.User.VoiceChannel);
-                       if (!audioPlaying) { sendAudioQueue(); }
+                       addAudioToQueue(command.Path, e.User.VoiceChannel, audioQueue);
+                       if (!audioPlaying) { sendAudioQueue(audioQueue); }
                        Console.WriteLine(string.Format("Received command: {1} from: {0}", e.User, command.Command));
                    }
 
@@ -100,10 +98,9 @@ namespace DCBot
         /// Send audio to the current _vClient channel
         /// </summary>
         /// <param name="path">Path of the .wav file to send</param>
-        private void send(string path)
+        private void send(string path, Command current, IAudioClient _vClient)
         {
             int blockSize = 3840; // The size of bytes to read per frame; 1920 for mono
-
             try
             {
                 using (FileStream f = File.Open(path, FileMode.Open))
@@ -121,19 +118,20 @@ namespace DCBot
             catch (Exception e) { Console.WriteLine(e); }
         }
 
-        private void addAudioToQueue(string path, Channel voiceChannel)
+        private void addAudioToQueue(string path, Channel voiceChannel, Queue<Command> audioQueue)
         {
             audioQueue.Enqueue(new Command(path, voiceChannel));
         }
 
-        private async void sendAudioQueue()
+        private async void sendAudioQueue(Queue<Command> audioQueue)
         {
+            IAudioClient _vClient = null;
             while (audioQueue.Count > 0)
             {
                 audioPlaying = true;
                 Command current = audioQueue.Dequeue();
                 try { _vClient = await current.VoiceChannel.JoinAudio(); } catch (Exception e) { Console.WriteLine(e); }
-                send(current.Path);
+                send(current.Path, current, _vClient);
             }
             await _vClient.Disconnect();
             audioPlaying = false;
